bb55cde6ee66498a118e01009d53c5c028ac26ee
KANTNOLI <zahargysev02@gmail.com>
2024-06-10 23:11:28 +0300

    Ready link DB + SOCKETS. work chats

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,8 @@ const localIP = Object.values(os.networkInterfaces())
 const app = express();
 const dbApp = express();
 
-const socketPort = process.env.PORT || 3001;
-const dbPort = process.env.PORT || 5001;
+const socketPort = process.env.PORT_SOCKET || 3001;
+const dbPort = process.env.PORT_DB || 5001;
 const bd = pg()({
   user: "postgres",
   host: "localhost",
@@ -23,6 +23,7 @@ const bd = pg()({
 });
 
 app.use(cors());
+dbApp.use(cors());
 const server = http.createServer(app);
 
 //Работа с сокетами
@@ -32,37 +33,53 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
+
 io.on("connection", (socket) => {
-  socket.on("join", (props) => {
-    socket.join(props.room);
-    io.to(props.room).emit("send", `Join ${props.name}`);
-    console.log(props.room + " " + props.name);
+  socket.on("join", (data) => {
+    socket.join(data);
   });
 
   socket.on("send", (data) => {
-    socket.to(data.room).emit("give", JSON.parse(data.data));
-    console.log(data.room + " mess " + data.data);
+    const rand = Math.random() * (100000 - 0) + 0;
+
+    bd.query(
+      `insert into messages values (${rand}, ${data.chatid}, ${data.senderid}, '${data.content}')`
+    );
+    socket.to(data.room).emit("give", {name: data.room, id: data.chatid});
   });
 });
+
 server.listen(socketPort, () => {
   console.log(`Сервер запущен! Путь ${localIP}:${socketPort}`);
 });
 //Работа с сокетами
-
 
 //Работа с Базой данных
 dbApp.listen(dbPort, async () => {
   console.log(`Сервер запущен! Путь ${localIP}:${dbPort}`);
 });
 
-dbApp.get("/api1", (req, res) => {
-  bd.query("SELECT * FROM users")
+dbApp.get("/users", (req, res) => {
+  const userName = req.query.name ? req.query.name : "";
+
+  bd.query(
+    `
+    SELECT users.id, users.name, users.userName, chat_participants.chatid, chat_participants, chats.name FROM users
+    JOIN chat_participants ON users.id = chat_participants.userId
+    JOIN chats ON chat_participants.chatId = chats.id
+    WHERE users.userName = '@${userName}'`
+  )
     .then((data) => res.status(200).json(data))
     .catch((error) => res.status(500).json({ error: error.message }));
 });
 
-dbApp.get("/api2", (req, res) => {
-  bd.query("SELECT * FROM messages")
+dbApp.get("/messages", (req, res) => {
+  const id = req.query.id ? req.query.id : 0;
+
+  bd.query(
+    `select * from messages
+     where chatid = ${id}`
+  )
     .then((data) => res.status(200).json(data))
     .catch((error) => res.status(500).json({ error: error.message }));
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,89 +6,94 @@ import { useEffect, useState } from 'react';
 const socket = io.connect("http://localhost:3001")
 
 function App() {
+  const [user, setUser] = useState('')
+  const [chatId, setChatId] = useState(0)
+  const [room, setRoom] = useState('room1')
+  const [messeage, setMesseages] = useState('')
 
-  const [rooms, setRooms] = useState(0)
-
-  
-  const [input, setInput] = useState('')
-  let name = `Name${Math.random() * (100 - 0) + 0}`;
-  let room = "room1"
-
-  const [data, setData] = useState('')
-
-  let join = () => {
-    socket.emit("join", {
-      room: room,
-      name: name,
-      messeage: `Зашел ${name}`,
-    })
+  //инпуты
+  const [inputMessage, setInputMessage] = useState('')
+  function setMessage(event) {
+    setInputMessage(event.target.value)
   }
+  const [inputName, setInputName] = useState('test1')
+  function setName(event) {
+    setInputName(event.target.value)
+  }
+  //инпуты
+
 
   useEffect(() => {
-    socket.on("join", (props) => {
-      data ?
-        setData([...data, props]) : setData(props)
-    })
+    axios.get(`http://localhost:5001/users?name=${inputName}`)
+      .then((response) => {
+        setUser(response.data)
+      })
 
     socket.on("give", (props) => {
-      console.log(props);
-      data ?
-        setData([...data, props]) : setData(props)
+      join(props.name, props.id)
     })
 
 
-    axios.get('http://localhost:5001/api2')
-    .then(function (response) {
-      setRooms(response.data)
-      console.log(response.data);
-    })
 
-  }, [socket])
+  }, [inputName, socket])
 
   const send = () => {
     socket.emit("send", {
+      //id: 1,
+      chatid: messeage[0].chatid,
+      senderid: user[0].id,
+      content: inputMessage,
+      metadata: null,
+      //
       room: room,
-      data: JSON.stringify([...data, {
-        room: room,
-        name: name,
-        messeage: input,
-      }]),
-      name: name
     })
-    setData([...data, {
-      room: room,
-      name: name,
-      messeage: input,
-    }])
-    setInput("")
+    setInputMessage("")
+    setMesseages([
+      ...messeage,
+      {
+        chatid: messeage[0].chatid,
+        senderid: user[0].id,
+        content: inputMessage,
+        metadata: null
+      }]
+    )
   }
 
-  function a(event) {
-    setInput(event.target.value)
+
+
+
+  let join = (name, id) => {
+    socket.emit("join", name)
+    setChatId(id)
+    setRoom(name)
+    axios.get(`http://localhost:5001/messages?id=${id}`)
+      .then((response) => {
+        setMesseages(response.data)
+      })
   }
 
   return (
     <div>
-      <button onClick={() => join()}>Join</button>
+      <input className='test' onChange={() => setName(event)} value={inputName} type="text" placeholder="name" />
       <div className='a'>
         <div className='chats'>
-          <div className="chat">s</div>
-          <div className="chat">s</div>
-          <div className="chat">s</div>
-        </div>
-
-        <div className='content'>
-          {data &&
-            data.map((user, id) => (
-              user ?
-                <div key={id} className={user === name ? "messeage1" : "messeage2"}>
-                  <p>{user.name}</p>
-                  <p>{user.messeage}</p>
-                </div> : ""
+          {user &&
+            user.map((chat, id) => (
+              <div onClick={() => join(chat.name, chat.chatid)} key={id} className="chat">{chat.name}</div>
             ))
           }
         </div>
-        <input onChange={() => a(event)} value={input} type="text" />
+
+        <div className='content'>
+          {messeage && user[0] &&
+            messeage.map((message, id) => (
+              <div key={id} className={message.senderid != user[0].id ? "messeage1" : "messeage2"}>
+                <p>{message.content}</p>
+              </div>
+            ))
+          }
+        </div>
+        <input onChange={() => setMessage(event)} value={inputMessage} type="text" />
         <button onClick={() => send()} className='send'>send</button>
       </div>
     </div>
